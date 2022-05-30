862c7ef2e240ba2c9e2c61acd52fae49a47f9467
Kaushik <rknarayan02@gmail.com>
2022-05-30 16:04:23 +0530

    getting ready for heroku deployment

--- config/config.js
+++ config/config.js
@@ -1,33 +1,30 @@
 require('dotenv').config();
 module.exports = {
-    "development": {
-        "username": process.env.DEV_USERNAME, // local postgres DB username
-        "password": process.env.DEV_PASSWORD, // local postgres DB password
-        "database": "ctf",
-        "host": "127.0.0.1",
-        "dialect": "postgres", // DO NOT CHANGE: array data type is used
-        "query":    {
-            "raw": true,
-        },
-    },
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "postgres", // DO NOT CHANGE: array data type is used
-        "query":    {
-            "raw": true,
-        },
-    },
-    "production": {
-        "username": process.env.PROD_USERNAME,
-        "password": process.env.PROD_PASSWORD,
-        "database": "ctf",
-        "host": process.env.PROD_HOST,
-        "dialect": "postgres", // DO NOT CHANGE: array data type is used
-        "query":    {
-            "raw": true,
-        },
-    }
+	"development": {
+		"username": process.env.DEV_USERNAME, // local postgres DB username
+		"password": process.env.DEV_PASSWORD, // local postgres DB password
+		"database": "ctf",
+		"host": "127.0.0.1",
+		"dialect": "postgres", // DO NOT CHANGE: array data type is used
+		"query": {
+			"raw": true,
+		},
+	},
+	"test": {
+		"username": "root",
+		"password": null,
+		"database": "database_test",
+		"host": "127.0.0.1",
+		"dialect": "postgres", // DO NOT CHANGE: array data type is used
+		"query": {
+			"raw": true,
+		},
+	},
+	"production": {
+		"use_env_variable": "DATABASE_URL",
+		"dialect": "postgres", // DO NOT CHANGE: array data type is used
+		"query": {
+			"raw": true,
+		},
+	}
 };
